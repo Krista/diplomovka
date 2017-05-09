b649b5fef6d52cf1621ec9641db0fe89b56a351c
Krista <komanova4@uniba.sk>
2017-05-09 13:22:51 +0200

    delete wrong files

--- change_to_oposite.m
+++
@@ -1,4 +0,0 @@
-function [s,vector] = change_to_oposite (i, vector, data)
-  vector(i) *= -1;
-  s = score(vector, data);
-endfunction
--- swap.m
+++
@@ -1,5 +0,0 @@
-function [s, vector] = swap (i1, i2, vector, data)
-  vector(i1) *= -1;
-  vector(i2) *= -1;
-  s = score(vector, data);
-endfunction
